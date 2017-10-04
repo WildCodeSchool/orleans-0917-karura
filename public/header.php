a1473e8c0cbbc1da9c4db22d3dfebd3cb7365cf0
NoclainB <benjamin.noclain@gmail.com>
2017-10-04 15:05:18 +0200

    Header (barre de navigation) + images

--- public/header.php
+++ public/header.php
@@ -1,7 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Karuka</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/style.css">
+
+</head>
+
+<!-- Start barre de navigation-->
+
+<header>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <img class="img-responsive" src="../src/images/logo/karuralogosm.png" alt="karura">
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <form class="navbar-form navbar-right">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Rechercher">
+                    </div>
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+                </form>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">Accueil <span class="sr-only">(current)</span></a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Modèle <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Natation Synchronisée</a></li>
+                            <li><a href="#">Twirling</a></li>
+                            <li><a href="#">Natation</a></li>
+                            <li><a href="#">GR</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Accessoires</a></li>
+                    <li><a href="#">Contactez</a></li>
+                    <li><a href="#">A propos</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<!-- End barre de navigation-->
+
+<!--Include de la page index.php-->
 <?php
-/**
- * Created by PhpStorm.
- * User: noclain
- * Date: 03/10/17
- * Time: 09:49
- */
+
+include 'index.php';
+
+?>
+
+<?php
+
+include 'footer.php';
+
+?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
