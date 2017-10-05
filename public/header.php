dec9f8c7bf6259dcd55790faea52cd13a5a0e168
NoclainB <benjamin.noclain@gmail.com>
2017-10-05 11:17:20 +0200

    Merge Sprint_1

--- public/header.php
+++ public/header.php
@@ -58,17 +58,6 @@
 <!-- End barre de navigation-->
 
 <!--Include de la page index.php-->
-<?php
 
-include 'index.php';
 
-?>
 
-<?php
-
-include 'footer.php';
-
-?>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
