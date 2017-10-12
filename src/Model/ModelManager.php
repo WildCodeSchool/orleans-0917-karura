bb4da4a7035aecd123ae8d3cf25637e4681f4f1a
Arnaud Gillardin <arnaudgill89@live.fr>
2017-10-12 20:18:42 +0200

    structuring MVC for simple search about model without declination link

--- src/Model/ModelManager.php
+++ src/Model/ModelManager.php
@@ -26,6 +26,7 @@ class ModelManager
         $statement = $this->pdo->query($req);
         return $statement->fetchAll(\PDO::FETCH_CLASS, \Karura\Model\Category::class);
     }
+
     public function find(int $id)
     {
         $req = "SELECT * FROM " . self::TABLE . " WHERE id=:id";
@@ -34,6 +35,35 @@ class ModelManager
         $statement->execute();
         $model = $statement->fetchAll(\PDO::FETCH_CLASS, \Karura\Model\Category::class);
         return $model[0];
+    }
+
+    /**
+     * Function for simple form search by model name
+     * @param int $id
+     * @return mixed
+     */
+    public function findByName(string $modelName)
+    {
+        $req = "SELECT * 
+                FROM " . self::TABLE . "
+                WHERE name LIKE :name";
+        $statement = $this->pdo->prepare($req);
+        $statement->bindValue('name', '%'.$modelName.'%', \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll(\PDO::FETCH_CLASS, \Karura\Model\Category::class);
+    }
+
+    public function findByNameWithCategory(string $modelName, Category $category)
+    {
+        $req = "SELECT * 
+                FROM " . self::TABLE . "
+                WHERE name LIKE :name
+                AND category_id=:category_id";
+        $statement = $this->pdo->prepare($req);
+        $statement->bindValue('name', '%'.$modelName.'%', \PDO::PARAM_INT);
+        $statement->bindValue('category_id', $category->getId(), \PDO::PARAM_INT);
+        $statement->execute();
+        return $statement->fetchAll(\PDO::FETCH_CLASS, \Karura\Model\Category::class);
     }
 
     public function insert()
