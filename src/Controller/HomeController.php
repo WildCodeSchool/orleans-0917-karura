6b2ec3bcc3c9297ff6ded35212412c7d1307bfd0
fsacado <loann.meignant@hotmail.fr>
2017-10-19 11:33:48 +0200

    Resolving conflicts

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -43,6 +43,54 @@ var_dump($declinationsByCat);
 <<<<<<< Updated upstream
         //
         return $this->twig->render('contact.html.twig');
+=======
+        $errors = [];
+        if (!empty($_POST)) {
+
+            if (empty($_POST['formLastName'])) {
+                $errors['formLastName'] = "Merci de renseigner votre nom";
+            }
+            if (empty($_POST['formMail'])) {
+                $errors['formMail'] = "Merci de renseigner votre email";
+            }
+            if (empty($_POST['formMessage'])) {
+                $errors['formMessage'] = "Merci d'écrire un message";
+            }
+
+
+            if (count($errors) == 0) {
+
+                $sendTo = "loann.meignant@hotmail.fr";
+                $from = $_POST['formMail'];
+                $gender = $_POST['gender'];
+                $firstName = $_POST['formFirstName'];
+                $lastName = $_POST['formLastName'];
+                $message = $_POST['formMessage'];
+
+                $subject = "Envoi de message sur Karura.com";
+                $header = 'De ' . $from;
+
+                $messageSent = $gender . ' ' . $firstName . ' ' . $lastName . " vous a envoyé un message sur Karura.com :" . "\r\n" . $message;
+
+//                var_dump($sendTo);
+//                var_dump($subject);
+//                var_dump($messageSent);
+//                var_dump($header);
+
+                sendmail($sendTo, $subject, $messageSent, $header);
+
+                var_dump(mail($sendTo, $subject, $messageSent, $header));
+
+
+                //INSERER LE MESSAGE "BIEN ENVOYÉ" SUR LA PAGE DE REDIRECTION
+//                header('Location: index.php');
+            }
+
+        }
+        return $this->twig->render('contact.html.twig', [
+            'errors' => $errors,
+        ]);
+>>>>>>> Stashed changes
     }
 
     /**
