4d5f4fe329baac746be6346d932be9edf5bc7ff8
fsacado <loann.meignant@hotmail.fr>
2017-10-20 10:42:11 +0200

    Commit changes style and form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -112,39 +112,41 @@ class HomeController extends Controller
 
                 if (!empty($_FILES)) {
                     if ($_FILES['formFile']['error'] !== 4) {
-                        if (filesize($_FILES['formFile']['tmp_name']) < 26214400) {
+                        if (filesize($_FILES['formFile']['tmp_name']) > 26214400) {
+                            $errors['formFile'] = "Votre fichier est trop volumineux";
+                        } else {
                             $attachment = \Swift_Attachment::fromPath($_FILES['formFile']['tmp_name'])->setFilename($_FILES['formFile']['name']);
                             $message->attach($attachment);
                         }
                     }
                 }
-
-                $mailer->send($message);
-
-                $messageAccusingReception = (new \Swift_Message($header))
-                    ->setFrom([$setTo])
-                    ->setTo([$setFrom => $firstName])
-                    ->setBody('Nous avons bien reçu votre message, et vous répondrons dans les meilleurs délais.' . "\r\n" . 'Belle journée à vous' . "\r\n\r\n" . 'Message envoyé : ' . "\r\n" . $formMessage);
-
-                $mailer->send($messageAccusingReception);
-
-
-                //INSERER LE MESSAGE "BIEN ENVOYÉ" SUR LA PAGE DE REDIRECTION
-//                header('Location: index.php');
             }
 
-        }
-        return $this->twig->render('contact.html.twig', [
-            'errors' => $errors,
-        ]);
-    }
+            $mailer->send($message);
 
-    /**
-     * @return string
-     */
-    public function showMentions()
-    {
-        // show mentions légales
-        return $this->twig->render('mentions.html.twig');
+            $messageAccusingReception = (new \Swift_Message($header))
+                ->setFrom([$setTo])
+                ->setTo([$setFrom => $firstName])
+                ->setBody('Nous avons bien reçu votre message, et vous répondrons dans les meilleurs délais.' . "\r\n" . 'Belle journée à vous' . "\r\n\r\n" . 'Message envoyé : ' . "\r\n" . $formMessage);
+
+            $mailer->send($messageAccusingReception);
+
+
+            //INSERER LE MESSAGE "BIEN ENVOYÉ" SUR LA PAGE DE REDIRECTION
+//                header('Location: index.php');
+        }
+
     }
+return $this->twig->render('contact.html.twig', ['errors' => $errors,]);
+}
+
+/**
+ * @return string
+ */
+public
+function showMentions()
+{
+    // show mentions légales
+    return $this->twig->render('mentions.html.twig');
+}
 }
