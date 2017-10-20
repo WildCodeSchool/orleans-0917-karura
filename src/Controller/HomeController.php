d60afffc71bd6ed2408c57f0f7da420416940a01
fsacado <loann.meignant@hotmail.fr>
2017-10-20 13:26:16 +0200

    Modif phrase accusé récéption envoi mail

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -126,7 +126,7 @@ class HomeController extends Controller
                 $messageAccusingReception = (new \Swift_Message($header))
                     ->setFrom([$setTo])
                     ->setTo([$setFrom => $firstName])
-                    ->setBody('Nous avons bien reçu votre message, et vous répondrons dans les meilleurs délais.' . "\r\n" . 'Belle journée à vous' . "\r\n\r\n" . 'Message envoyé : ' . "\r\n" . $formMessage);
+                    ->setBody('Nous avons bien reçu votre message, et vous répondrons dans les meilleurs délais.' . "\r\n" . 'Belle journée à vous.' . "\r\n\r\n" . 'Message envoyé : ' . "\r\n" . $formMessage);
 
                 $mailer->send($messageAccusingReception);
 
